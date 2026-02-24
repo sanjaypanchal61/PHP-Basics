de7853660342f07144f492b2011c97fd40d42506
sanjaypanchal61 <sanjaypanchal6122@gmail.com>
2026-02-24 23:39:24 +0530

    learn variable

---
+++ 1_variable/README.md
@@ -0,0 +1,32 @@
+# 📘 PHP Learning – Day 1
+
+## 📌 Topic: Variables in PHP
+
+Today I learned the basics of PHP Variables.
+
+---
+
+## 🔹 What is a Variable?
+
+A variable is used to store data in PHP.
+
+---
+
+## 🔹 Rules of PHP Variables
+
+- Must start with $
+- Cannot start with number
+- Case-sensitive
+- End with semicolon ;
+
+---
+
+## 🔹 Data Types Learned
+
+- String
+- Integer
+- Float
+- Boolean
+- Array
+
+---
---
+++ 1_variable/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    // $variableName = value;
+
+    // string
+    $name = "Roy";
+
+    // integer
+    $age = 20;
+
+    // float
+    $price = 99.99;
+
+    // boolean
+    $isActive = true;
+
+    // array
+    $colors = ["red", "blue", "green"];
+
+    echo "My name is " . $name . " and age is " . $age;
+
+    ?>
+</body>
+</html>
