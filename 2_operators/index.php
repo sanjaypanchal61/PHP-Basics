46268fb79ebceb79449e4cf5fc2d9b6f0acb0cdf
sanjaypanchal61 <sanjaypanchal6122@gmail.com>
2026-02-25 23:49:38 +0530

    learn operators

---
+++ 2_operators/index.php
@@ -0,0 +1,40 @@
+<?php
+
+// Arithmetic Operators
+$a = 10;
+$b = 3;
+
+echo $a + $b;
+echo $a - $b;  
+echo $a * $b;  
+echo $a / $b;  
+echo $a % $b;  
+
+// Assignment Operators
+$x = 10;
+
+$x += 5;
+$x -= 3; 
+$x *= 3; 
+$x /= 3; 
+
+// Comparison Operators
+$a = 10;
+$b = "10";
+
+var_dump($a == $b);
+var_dump($a === $b);
+
+// Logical Operators
+$age = 20;
+
+if ($age > 18 && $age < 25) {
+    echo "Young Adult";
+}
+
+// Increment / Decrement
+$x = 5;
+$x++;
+$x--; 
+
+?>
